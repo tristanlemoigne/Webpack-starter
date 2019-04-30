a456e5c4e26a824b183fa13d2aecc71e7d292a35
Tristan Le Moigne <tristanlemoigne@gmail.com>
2019-04-30 14:53:01 +0200

    Adding developpement mode

--- webpack.config.js
+++ webpack.config.js
@@ -4,9 +4,14 @@ const CleanWebpackPlugin = require('clean-webpack-plugin');
 // const Fiber = require('fibers');
 
 module.exports = {
+    mode: 'development',
     entry: {
         app: './src/index.js',
         print: './src/print.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist'
     },
     plugins: [
         new CleanWebpackPlugin(),
